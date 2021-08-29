178a3db4cd0382696791e6d3a5ad71ac1f848397
Tony <917587892@qq.com>
2021-08-29 10:38:30 +0800

    更新

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,62 @@
+<template>
+  <swiper ref="swiper" v-if="banners.length">
+    <swiper-item v-for="(item,index) in banners">
+      <a >
+        <img :src="item.image" alt="" @load="imageLoaded">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: "HomeSwiper",
+    props: {
+      banners: {
+        type: Array,
+        required: true
+      },
+       banners2 :[{
+              image:'../../assets/img/home/1.jpg'
+        },{
+
+        },{
+           image:'../../assets/img/home/1.jpg'
+        },{
+           image:'../../assets/img/home/1.jpg'
+        }]
+
+    },
+    data() {
+    	return {
+    		isLoaded: false,
+
+      }
+    },
+    components: {
+      Swiper,
+      SwiperItem
+    },
+    methods: {
+      startTimer() {
+	      this.$refs.swiper && this.$refs.swiper.startTimer()
+      },
+      stopTimer() {
+        this.$refs.swiper && this.$refs.swiper.stopTimer()
+      },
+	    imageLoaded() {
+        console.log(this.banners);
+      	if (!this.isLoaded) {
+      		this.$emit('swiperLoaded')
+          this.isLoaded = true
+        }
+	    }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
